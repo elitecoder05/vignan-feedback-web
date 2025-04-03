ba43fcbe0ae01739b8d8c8bdaa5076c5e6fc2f55
elitecoder05 <varma.gurijala@gmail.com>
2025-04-03 15:57:22 +0530

    awesome

--- src/App.js
+++ src/App.js
@@ -131,6 +131,9 @@ function Home() {
             <option value="2">2</option>
             <option value="3">3</option>
             <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
           </select>
         </div>
 
